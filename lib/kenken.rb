24d477ce7045cf64b9eccb612b6bdb8df1d96500
Kentaro Kuribayashi <kentarok@gmail.com>
2019-09-04 19:57:07 +0900

    bunlde gem kenken

---
+++ lib/kenken.rb
@@ -0,0 +1,6 @@
+require "kenken/version"
+
+module Kenken
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/kenken_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class KenkenTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Kenken::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
